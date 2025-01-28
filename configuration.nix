7cc7f0ddb8f43fe2c2f5c8a474bad9f725e6ad83
Noon van der Silk <noon.vandersilk@iohk.io>
2025-01-28 10:32:58 +0000

    Use right hydra-version for the scripts

--- configuration.nix
+++ configuration.nix
@@ -25,7 +25,7 @@ let
 
   # This is used to get the script tx id, and should then agree with the
   # version that comes in via the flake input.
-  hydraVersion = "0.20.0";
+  hydraVersion = "0.19.0-634-g2b465027ef";
 
   # These three variables must agree
   networkName = "preview";
