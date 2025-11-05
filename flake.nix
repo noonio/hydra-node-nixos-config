20a7d5743d05aa16121cfe72082abbb82d1947ce
Noon van der Silk <noon.vandersilk@iohk.io>
2025-11-05 09:09:09 +0000

    nixpkgs update

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nixos-generators = {
       url = "github:nix-community/nixos-generators";
       inputs.nixpkgs.follows = "nixpkgs";
